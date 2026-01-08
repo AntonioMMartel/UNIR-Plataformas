0a2e82ff15b593657442aa6562076a95c7d44d20
Antonio ML <AntonioMMartel@gmail.com>
2026-01-08 19:45:40 +0000

    Lógica de salto (y mini saltito)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float moveSpeed = 5f;
+
+    [Header("Jump")]
+    [SerializeField] float jumpPower = 5f;
+
+    [Header("JumpCheck")]
+    private Transform groundCheckPos;
+    private Vector2 groundCheckSize = new Vector2(0.5f, 0.05f); // Este ti
+    private LayerMask groundLayer; // Este para mirar quién tiene la tag ground
+
 
     float horizontalMovement;
     // Start is called before the first frame update
@@ -25,4 +35,19 @@ public class PlayerMovement : MonoBehaviour
         horizontalMovement = context.ReadValue<Vector2>().x;
     }
 
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        } else if (context.canceled) // Salto chiquitin
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    private void OnDrawGizmosSelected() // Quiero ver el groundCheck
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
 }
