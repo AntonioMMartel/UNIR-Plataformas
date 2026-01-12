3e00f7cdee8a864f31b2947d57a0ab9501fc9e70
Antonio ML <AntonioMMartel@gmail.com>
2026-01-12 14:11:52 +0000

    Implemented flags as a way to advance through levels

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour, IItem
+{
+    public static event Action OnFlagCollect;
+    public void Collect()
+    {
+        OnFlagCollect.Invoke();
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -10,12 +11,33 @@ public class GameManager : MonoBehaviour
     public int score;
     [SerializeField] TMP_Text scoreText;
 
+    [SerializeField] List<GameObject> levels;
+    private int currentLevel = 0;
+    [SerializeField] GameObject nextLevelCanvas;
+    [SerializeField] GameObject UI;
+
+    private bool waitingForPlayerInput = false;
+
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
         scoreText.text = "0";
         Coin.OnCoinCollect += IncreaseScore; // Suscripción a CoinHub
+        Flag.OnFlagCollect += LoadNextLevel; // Suscripción a OnlyFlags
+        nextLevelCanvas.SetActive(false);
+        UI.SetActive(true);
+    }
+
+    void LoadNextLevel()
+    {
+        UI.SetActive(false);
+        nextLevelCanvas.SetActive(true);
+        Time.timeScale = 0f;
+
+        waitingForPlayerInput = true;
+
+
     }
 
     void IncreaseScore(int points)
@@ -27,6 +49,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (waitingForPlayerInput)
+        {
+            if (Input.anyKeyDown) // Me da pereza poderosa hacerlo con el InputSystem (lo siento jefe!)
+            {
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                waitingForPlayerInput = false;
+                nextLevelCanvas.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
     }
 }
