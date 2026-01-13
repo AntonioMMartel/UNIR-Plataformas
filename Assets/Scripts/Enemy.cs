e8612cc60227eba5efbe2917620849d4688784e7
Antonio ML <AntonioMMartel@gmail.com>
2026-01-13 20:07:44 +0000

    Added frog enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/Scripts/FrogEnemy.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogEnemy : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float horizontalSpeed = 2f;
+    [SerializeField] float jumpForce = 2f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheckPos;
+    [SerializeField] Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+
+    [SerializeField] float detectionRange = 10f;
+
+    [SerializeField] Animator animator;
+
+    [SerializeField] float jumpTimer;
+    private float currentJumpTime;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool shouldJump;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentJumpTime = jumpTimer;
+        
+    }
+
+    void Update()
+    {
+        GroundCheck();
+        animator.SetBool("isGrounded", isGrounded);
+        if (isGrounded && currentJumpTime < 0)
+        {
+            rb.velocity = new Vector2(0, 0);
+            currentJumpTime = jumpTimer;
+            shouldJump = true;
+        }
+    }
+
+    private void GroundCheck()
+    {
+        // Raycast no funciona porque se proyecta desde el centro de la transform
+        // Necesitaría dos raycasts y ajustarlos con mucho sufrimiento -> mejor hitbox
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
+    }
+    private void OnDrawGizmosSelected() // Quiero ver el groundCheck
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    private void FixedUpdate()
+    {
+        currentJumpTime-= Time.deltaTime;
+        if (isGrounded && shouldJump)
+        {
+            shouldJump = false;
+
+            Vector2 direction = player.position - transform.position;
+
+            if (-detectionRange > direction.magnitude || direction.magnitude < detectionRange)
+            {
+                if (direction.x >= 0)
+                {
+                    rb.AddForce(new Vector2(horizontalSpeed, jumpForce), ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(new Vector2(-horizontalSpeed, jumpForce), ForceMode2D.Impulse);
+                }
+            }
+        }   
+    }
+}
